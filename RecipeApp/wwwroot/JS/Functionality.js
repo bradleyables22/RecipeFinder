3af307f2f12636de592aa1bfe672eccec5bf40f9
bradley <bradley.ables.ctr@us.af.mil>
2023-05-09 21:36:26 -0400

    loading screen coolness

--- RecipeApp/wwwroot/JS/Functionality.js
+++ RecipeApp/wwwroot/JS/Functionality.js
@@ -12,6 +12,20 @@
         });
     }, speed * 2);
 }
+function oppositeMoveBar(elementSelector) {
+    var $bar = $(elementSelector);
+    var speed = 1000;
+
+    setInterval(function () {
+        $bar.animate({
+            "top": "-=840"
+        }, speed, function () {
+            $bar.animate({
+                "top": "+=800"
+            }, speed);
+        });
+    }, speed * 2);
+}
 function slideIn(elementSelector) {
     $(elementSelector).slideDown(2000);
 }
